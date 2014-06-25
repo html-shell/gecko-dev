3f4f03052cc7f2f6be1e16d83113d91cfaa717f9
Yonggang Luo <luoyonggang@gmail.com>
2014-06-25 19:01:41 +0800

    Use find_packages to fixup the loss of mach and also recursively install all packages.

--- python/mozbuild/setup.py
+++ python/mozbuild/setup.py
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from setuptools import setup
+try:
+    from setuptools import setup,find_packages
+except:
+    from distutils.core import setup,find_packages
 
 VERSION = '0.1'
 
@@ -10,6 +13,6 @@ setup(
     name='mozbuild',
     description='Mozilla build system functionality.',
     license='MPL 2.0',
-    packages=['mach', 'mozbuild', 'mozpack'],
+    packages=find_packages(),
     version=VERSION
 )
