563baa6fbfdaddf58e077763ffe6feb25495c2a8
Yonggang Luo <luoyonggang@gmail.com>
2014-09-04 01:58:10 +0800

    Add a cell/column/row type TYPE_RENDERER and correspond CSS selector :-moz-tree-renderer

--- layout/xul/tree/nsTreeColumns.cpp
+++ layout/xul/tree/nsTreeColumns.cpp
@@ -321,11 +321,12 @@ nsTreeColumn::Invalidate()
   // Figure out our column type. Default type is text.
   mType = nsITreeColumn::TYPE_TEXT;
   static nsIContent::AttrValuesArray typestrings[] =
-    {&nsGkAtoms::checkbox, &nsGkAtoms::progressmeter, nullptr};
+    {&nsGkAtoms::checkbox, &nsGkAtoms::progressmeter, &nsGkAtoms::renderer, nullptr};
   switch (mContent->FindAttrValueIn(kNameSpaceID_None, nsGkAtoms::type,
                                     typestrings, eCaseMatters)) {
     case 0: mType = nsITreeColumn::TYPE_CHECKBOX; break;
     case 1: mType = nsITreeColumn::TYPE_PROGRESSMETER; break;
+    case 2: mType = nsITreeColumn::TYPE_RENDERER; break;
   }
 
   // Fetch the crop style.
