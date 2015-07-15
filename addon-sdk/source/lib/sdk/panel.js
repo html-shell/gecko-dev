c4f85bcf5a4ab71f328cbeb4856fc7be8cfdacbd
Yonggang Luo <luoyonggang@gmail.com>
2015-07-16 05:23:56 +0800

    Enable the panel.

--- addon-sdk/source/lib/sdk/panel.js
+++ addon-sdk/source/lib/sdk/panel.js
@@ -9,7 +9,8 @@ module.metadata = {
   "stability": "stable",
   "engines": {
     "Firefox": "*",
-    "SeaMonkey": "*"
+    "SeaMonkey": "*",
+    "Thunderbird": "*",
   }
 };
 
@@ -147,7 +148,7 @@ const Panel = Class({
     }
 
     // Setup view
-    let view = domPanel.make();
+    let view = domPanel.make(null, options.enableChrome);
     panels.set(view, this);
     views.set(this, view);
 
